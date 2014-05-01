0d417e270f1137d04f4b198b32516227466a3ed8
jhugh <jhugh@github.com>
2014-05-01 10:08:56 +0100

    added Aria, added ProfileControllers

---
+++ Entaria/Controllers/AriaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Entaria.Controllers
+{
+    public class AriaController : Controller
+    {
+        //
+        // GET: /Aria/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Entaria/Controllers/LCHProfileController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Entaria.Models;
+
+namespace Entaria.Controllers
+{
+    public class LCHProfileController : Controller
+    {
+        private EntariaContext db = new EntariaContext();
+        //
+        // GET: /LCHProfile/
+
+        public ActionResult Index()
+        {
+            return View(db.LoyaltyCardHolders.ToList());
+        }
+
+    }
+}
