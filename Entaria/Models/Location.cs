6a65f443ea686f72db3df1fc8d6861afd3c7e19e
jhugh <jhugh@github.com>
2014-05-02 20:51:55 +0100

    notional change

--- Entaria/Models/Location.cs
+++ Entaria/Models/Location.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -27,3 +27,4 @@ namespace Entaria.Models
         public string LastModifiedByUserName { get; set; }
     }
 }
+
